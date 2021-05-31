50c48feb3581c93b7ce071a4fef3c161fec086b4
Vicky BRS <64796334+viki2711@users.noreply.github.com>
2021-05-31 14:08:06 -0400

    Update mail.js

--- mail.js
+++ mail.js
@@ -3,7 +3,7 @@ const mailGun = require('nodemailer-mailgun-transport');
 
 const auth = {
   auth: {
-    api_key: '52a19535a7e4c27e58349aa865bddc3a-1d8af1f4-3cf63c25',
+    api_key:,
     domain: 'sandbox7beccea89f514378b3aeeba98c7da11a.mailgun.org'
   }
 };
